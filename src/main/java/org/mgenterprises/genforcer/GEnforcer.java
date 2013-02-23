1e7f46ab5eab02a60f84d52a224021a39f204e7b
MANUEL <MANUEL@DESKTOP1-WIN8>
2013-02-23 13:26:42 -0500

    Initial

---
+++ src/main/java/org/mgenterprises/genforcer/GEnforcer.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mgenterprises.genforcer;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author MANUEL
+ */
+public class GEnforcer extends JavaPlugin{
+    
+}
